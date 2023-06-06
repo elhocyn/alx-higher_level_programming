9552910dc65c01473d29c6e78c5673a11334a4da
elhocyn <houcinemfy1997@gmail.com>
2023-06-06 16:30:05 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            print("{}{}, ".format(i, j), end="")
+            print("{}{}, ".format(i,j), end="")
 print("89")
