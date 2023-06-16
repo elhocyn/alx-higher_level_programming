b39d26bef5c7361c9fb20002c755e101e41d128d
elhocyn <houcinemfy1997@gmail.com>
2023-06-16 08:45:17 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
+def calculate_subtraction(subtraction_list):
+    total_subtraction = 0
+    max_value = max(subtraction_list)
+
+    for num in subtraction_list:
+        if max_value > num:
+            total_subtraction += num
+
+    return max_value - total_subtraction
+
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+
     if not isinstance(roman_string, str):
         return 0
-    if not roman_string.isupper():
-        roman_string.upper()
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numeral_keys = list(roman_numerals.keys())
 
     result = 0
-    temp = list(roman_string)
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
+    last_numeral = 0
+    subtraction_list = [0]
+
+    for char in roman_string:
+        for numeral in numeral_keys:
+            if numeral == char:
+                if roman_numerals.get(char) <= last_numeral:
+                    result += calculate_subtraction(subtraction_list)
+                    subtraction_list = [roman_numerals.get(char)]
+                else:
+                    subtraction_list.append(roman_numerals.get(char))
+
+                last_numeral = roman_numerals.get(char)
+
+    result += calculate_subtraction(subtraction_list)
+
     return result
--- 0x04-python-more_data_structures/12-test.py
+++ 0x04-python-more_data_structures/12-test.py
@@ -1,56 +1,19 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        roman_string.upper()
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-    result = 0
-    temp = list(roman_string)
-    itemp = []
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            # If a bigger letter preceedes a smaller letter
-            # the letters are added
-            try:
-                l = r_dict[temp[idx]]
-                r = r_dict[temp[idx + 1]]
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-                if l > r:
-                    itemp.append(l + r)
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-                    # When a smaller letter preceedes bigger one,
-                    # the letters are subtracted.
-                elif l < r:
-                    itemp.append(l - r)
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-                    # When a letter is repeated 2 or 3 times,
-                    # they get added together
-                elif l == r and r_dict[temp[idx + 2]] != l:
-                    itemp.append(l + r)
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-                    # The same letter cannot be used more than three times
-                    # in a succession.
-                elif 1 == r and r_dict[temp[idx + 2]] == r:
-                    itemp.append(sum([l,r, r_dict[temp[idx + 2]]]))
-                else:
-                    pass
-
-            except IndexError:
-                pass
-            idx += 1
-
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
