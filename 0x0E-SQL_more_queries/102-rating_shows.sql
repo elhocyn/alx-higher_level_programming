5f042b87c4f6121e290231ce96e385a4baa6fcb5
elhocyn <houcinemfy1997@gmail.com>
2023-08-16 16:43:46 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,9 @@
--- script that lists all shows from hbtn_0d_tvshows_rate
--- by their rating.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
+-- List all shows from 'hbtn_0d_tvshows_rate' by their rating
+-- Each record should display tv_shows.title, rating sum
+-- Results must be sorted in ascending order by the rating
+-- You can use only one SELECT statement
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_show_ratings.show_id
+ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,9 @@
---  script that lists all genres in the database
--- hbtn_0d_tvshows_rate by their rating.
-SELECT g.name AS genre_name, SUM(sr.rate) AS rating_sum
-FROM tv_genres g
-JOIN tv_show_genres sg ON g.id = sg.genre_id
-JOIN tv_show_ratings sr ON sg.show_id = sr.show_id
-GROUP BY g.name
-ORDER BY rating_sum DESC;
+-- List all genres in 'hbtn_0d_tvshows_rate' by their rating
+-- Each record should display tv_genres.name and rating sum
+-- You can use only one SELECT statement
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY rating DESC;
